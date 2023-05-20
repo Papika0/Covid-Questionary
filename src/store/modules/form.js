056aca5bfe4116d2c2a7d6ecac37120d437ccb0f
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-05-20 20:54:26 +0400

    feat - add rules/ refactor state and usage in first page

--- src/store/modules/form.js
+++ src/store/modules/form.js
@@ -1,10 +1,20 @@
+function getValueFromLocalStorage(key, defaultValue = '') {
+  const value = localStorage.getItem(key);
+  return value !== null ? value : defaultValue;
+}
+
 export default {
   namespaced: true,
   state: {
     formData: {
-      name: '',
-      surname: '',
-      email: '',
+      name: getValueFromLocalStorage('name'),
+      surname: getValueFromLocalStorage('surname'),
+      email: getValueFromLocalStorage('email'),
+      had_covid: getValueFromLocalStorage('had_covid'),
+      had_antibody_test: getValueFromLocalStorage('had_antibody_test'),
+      antibodies_test_date: getValueFromLocalStorage('antibodies_test_date'),
+      antibodies_number: getValueFromLocalStorage('antibodies_number'),
+      covid_sickness_date: getValueFromLocalStorage('covid_sickness_date'),
     },
   },
   mutations: {
@@ -12,10 +22,8 @@ export default {
       state.formData[payload.field] = payload.value;
       localStorage.setItem(payload.field, payload.value);
     },
-    getDataFromLocalStorage(state) {
-      state.formData.name = localStorage.getItem('name');
-      state.formData.surname = localStorage.getItem('surname');
-      state.formData.email = localStorage.getItem('email');
-    }
-  }
+  },
+  getters: {
+    getAllFormData: (state) => state.formData
+  },
 };
