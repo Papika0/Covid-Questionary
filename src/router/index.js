78e2367180fdf7ebd768c73d13b6ea88ec12c8ec
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-05-22 22:36:08 +0400

    refactor - absolute path

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Identification from '../views/Identification.vue'
-import CovidQuestions from '../views/CovidQuestions.vue'
+import Home from '@/views/Home.vue'
+import Identification from '@/views/Identification.vue'
+import CovidQuestions from '@/views/CovidQuestions.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
