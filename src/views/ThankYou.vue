b77923161c38b37f34a966b34e5eac436371ff38
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-05-25 21:18:31 +0400

    feat - add thank you page/ icons/ font

---
+++ src/views/ThankYou.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="bg-my-black h-screen">
+        <div class="flex flex-col justify-center h-full max-w-md mx-auto">
+            <IconStarBig class="mb-8 ml-8" />
+            <p class="text-white text-6xl font-bold tracking-my-wide leading-main font-contractica mx-auto">მადლობა</p>
+            <IconStarSmall class="mx-auto ml-370" />
+        </div>
+    </div>
+</template>
+
+<script>
+import IconStarBig from '@/components/icons/IconStarBig.vue';
+import IconStarSmall from '@/components/icons/IconStarSmall.vue';
+
+export default {
+    components: {
+        IconStarBig,
+        IconStarSmall
+    }
+}
+</script>
