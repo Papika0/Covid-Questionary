24a9b8d1a02ca499c890f2740c60d14d733de648
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-05-25 21:15:03 +0400

    feat - add fifth page, routes, text area component

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,20 @@ export default {
     extend: {
       backgroundColor: {
         'primary': '#EAEAEA',
+        'my-button': '#208298',
+        'my-black': '#232323',
       },
-      fontFamily: { fontFamily: 'Anonymous Pro' },
+      fontFamily: {
+        fontFamily: 'Anonymous Pro',
+        contractica: ['TBC Contractica CAPS', 'sans-serif'],
+      },
       textColor: {
         'primary': '#232323',
         'my-grey': '#626262',
         "my-blue": "#1289AE",
       },
       width: {
-        '500' : '500px',
+        '500': '500px',
       },
       borderWidth: {
         '0.8': '0.8px',
@@ -29,6 +34,19 @@ export default {
       },
       accentColor: {
         'primary': '#232323',
+      },
+      maxWidth: {
+        '630': '630px',
+      },
+      lineHeight: {
+        'header': '27px',
+        "main": "77px",
+      },
+      letterSpacing: {
+        'my-wide': '0.24em',
+      },
+      margin: {
+        '370': '370px',
       },
     },
     plugins: [],
